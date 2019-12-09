94f99bc83cd555d6351c9fad2725bd2a4b5818d1
s-115-31 <s-115-31@gumiforest.ae.krakow.pl>
2019-12-09 14:39:33 +0100

    ZajeciaNr7

---
+++ 07-ObjectOrientedProgramming/Ex5.py
@@ -0,0 +1,16 @@
+class Songs():
+    def __init__(self, artist, title, album, year):
+        self.artist = artist
+        self.title = title
+        self.album = album
+        self.year = year
+    
+    def __str__(self):
+        return f'{"Wykonawca:":<11}' + self.artist + '\n' + f'{"Utwór:":<11}' + self.title + '\n' + f'{"Album:":<11}' + self.album + '\n' + f'{"Rok:":<11}' + self.year
+
+utwor1 = Songs('Dawid Podsiadło', 'Nie ma fal', 'Małomiasteczkowy', '2018')
+print(utwor1)
+utwor2 = Songs('Krzysztof Krawczyk', 'Mój przyjacielu', 'The Very Best Of..', '1999')
+print(utwor2)
+utwor3 = Songs('Kombi', 'Nasze pokolenie', 'c.d.', '2004')
+print(utwor3)
---
+++ 07-ObjectOrientedProgramming/Ex7.py
@@ -0,0 +1,17 @@
+class Student():
+    
+    licznik = 100000
+    university = "UEK Kraków"
+    
+    def __init__(self, name, surname, major):
+        self.name = name
+        self.surname = surname
+        self.major = major
+        self.album = Student.licznik
+        Student.licznik += 1
+    
+    def __str__ (self):
+        return self.name + ' ' + self.surname.upper() + f" ({self.album})," + f"{self.major}," + Student.university
+    
+uczen1 = Student("Marian", "Kowalski", "Informatyka Stosowana")
+print(uczen1)
